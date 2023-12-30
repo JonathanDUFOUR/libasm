4c7cd1cfdd4be4cf275f92f6927ef279fe35d69a
jodufour <jodufour@student.42.fr>
2023-12-30 02:23:12 +0100

    Write functions [DOING]   ft_read       [DOING]   ft_strcmp     [TODO]   ft_strcpy     [TODO]   ft_strdup     [TODO]   ft_strlen     [OK]   ft_write      [TODO]

--- src/ft_read.s
+++ src/ft_read.s
@@ -1,1 +1,22 @@
-; TODO
+section .data
+
+section .bss
+
+section .text
+	global ft_read
+
+; Reads N bytes from a file descriptor and store them in a buffer.
+; In case of any error, sets errno properly.
+;
+; Parameters
+; rdi: the file descriptor to read from
+; rsi: the address of the buffer to read into
+; rdx: the number of bytes to read
+;
+; Return
+; rax: the number of bytes read, or -1 if an error occurred
+
+ft_read:
+	mov rax, 0	; sys_read
+	syscall
+	; TODO: check for errors and set errno
--- src/ft_strlen.s
+++ src/ft_strlen.s
@@ -3,5 +3,22 @@ section .data
 section .bss
 
 section .text
-ft_strlen
-	; TODO
+	global ft_strlen
+
+; Calculates the length of a nul-terminated string
+;
+; Parameters
+; rdi: the address of the nul-terminated string to calculate the length of
+;
+; Return
+; rax: the length of string
+ft_strlen:
+	mov rax, rdi
+.loop:
+	cmp byte [rax], 0
+	je .end
+	inc rax
+	jmp .loop
+.end:
+	sub rax, rdi
+	ret
