6d55e5b21cbd7a90b2434daee01ff8f6705b340f
jodufour <jodufour@student.42.fr>
2024-01-15 01:38:06 +0100

    Write bonus functions [DOING]   ft_list_push_front  [OK]   ft_list_size        [DOING]   ft_list_sort        [TODO]   ft_list_remove_if   [TODO]

--- src/ft_list_size.s
+++ src/ft_list_size.s
@@ -1,1 +1,17 @@
+global ft_list_size
+
+section .data
+
+section .bss
+
+section .text
+; Calculates how many nodes a given list contains.
+;
+; Parameters:
+; rdi: the address of the first node of the list. (assumed to be a valid address or NULL)
+;
+; Return:
+; rax: the number of nodes in the list.
+ft_list_size:
 ; TODO
+ret
