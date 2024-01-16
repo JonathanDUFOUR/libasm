9648c254172b6ababf50664ea102fe3e56a27ffc
jodufour <jodufour@student.42.fr>
2024-01-16 16:38:33 +0100

    Write bonus functions [DOING]   ft_list_sort        [DOING]   ft_list_remove_if   [TODO]

--- src/ft_list_sort.s
+++ src/ft_list_sort.s
@@ -1,1 +1,16 @@
-; TODO
+global ft_list_sort
+
+section .data
+
+section .bss
+
+section .text
+; Sort the data of a given list in ascending order,
+; using a given function to compare data in the list.
+;
+; Parameters:
+; rdi: the address of a pointer to the first node of the list to sort. (assumed to be a valid address)
+; rsi: the address of a function to compare data in the list. (assumed to be a valid address)
+ft_list_sort:
+
+	retasm
